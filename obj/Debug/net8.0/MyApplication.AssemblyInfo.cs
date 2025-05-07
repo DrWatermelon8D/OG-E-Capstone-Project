2ce0c5f487daa900bb8a5257b2968bb73939ff4a
Max Gwartney <142606184+DrWatermelon8D@users.noreply.github.com>
2025-05-07 07:48:12 -0500

    fixed some merge errors

--- obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8040a72bfec4911b28f66511757ba915ea71fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79785129bc2d20c787f20798ecafb0de8f5d325a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
